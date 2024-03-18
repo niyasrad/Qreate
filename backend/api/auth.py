67bf9de32cf61a0ac0539f1c7b0960ef740ab1e3
Vaishnavi Manicka <76160783+Vaish24navi@users.noreply.github.com>
2024-03-18 16:19:26 +0530

    fix(backend): patch for several fixes (#8)

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,11 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, Request, status
 
 from core.utils.auth import create_access_token, get_password_hash, verify_password
-from core.utils.db import get_db
 from core.utils.errors import credential_error, handle_exception, conflict_error
+from core.utils.middlewares import authenticate_user
 from core.schema.brand import Brand,BrandAuth,BrandLogin
 
-from pymongo.database import Database
 from pymongo.collection import Collection
 
 router = APIRouter(
@@ -14,8 +13,8 @@ router = APIRouter(
 )
 
 
-@router.post("/login")
-async def login_user(user: BrandLogin, db: Database = Depends(get_db)):
+@router.post("/login", status_code=status.HTTP_200_OK)
+async def login_user(brand: BrandLogin, request: Request):
     """
     Brand login
 
@@ -27,24 +26,31 @@ async def login_user(user: BrandLogin, db: Database = Depends(get_db)):
     - dict: the response message and the access token
     """ 
     try:
-        users_collection: Collection = db.get_collection("users")
-        user_find = users_collection.find_one({"brand_email": user.brand_email})
+        brands_collection: Collection = request.app.brands_collection
+        brand_find = brands_collection.find_one({"brand_email": brand.brand_email})
     except Exception as e:
         raise handle_exception(e)
 
-    if not user_find or not verify_password(user.brand_password, user_find["brand_password"]):
+    if not brand_find or not verify_password(brand.brand_password, brand_find["brand_password"]):
         raise credential_error()       
     try:
-        access_token = create_access_token(data={"sub": str(user_find["_id"])})
+        access_token = create_access_token(
+            data={"brand_id": str(brand_find["_id"])}
+        )
         return {
             "message":"User logged in sucessfully!",
-            "access_token": access_token
+            "data": {
+                "access_token": access_token,
+                "_id": str(brand_find["_id"]),
+                "brand_name": brand_find["brand_name"],
+                "brand_email": brand_find["brand_email"]
+            }
         }
     except Exception as e:
         raise handle_exception(e)
     
-@router.post("/register")
-async def register_user(user: BrandAuth, db: Database = Depends(get_db)):
+@router.post("/register", status_code=status.HTTP_200_OK)
+async def register_user(brand: BrandAuth,request: Request):
     """
     Brand registration
 
@@ -56,39 +62,63 @@ async def register_user(user: BrandAuth, db: Database = Depends(get_db)):
     - dict: the response message and the access token
     """
     try:
-        users_collection: Collection = db.get_collection("users")
-        existing_user = users_collection.find_one({"brand_email": user.brand_email})
+        brands_collection: Collection = request.app.brands_collection
+        existing_brand = brands_collection.find_one({"brand_email": brand.brand_email})
     except Exception as e:
         raise handle_exception(e)
     
-    if existing_user:
+    if existing_brand:
         raise conflict_error("User")
     try: 
-        hashed_pwd = get_password_hash(user.brand_password)
-        user_dump = user.model_dump()
+        hashed_pwd = get_password_hash(brand.brand_password)
+        brand_dump = brand.model_dump()
         
         new_brand = Brand(
-            brand_name = user.brand_name,
-            brand_email = user.brand_email,
+            brand_name = brand.brand_name,
+            brand_email = brand.brand_email,
             brand_password = hashed_pwd
         )
 
-        inserted_user = users_collection.insert_one(new_brand.model_dump())
-        user_dump.pop("brand_password")
-        inserted_user_data = {"_id": str(inserted_user.inserted_id), **user_dump}
+        inserted_user = brands_collection.insert_one(new_brand.model_dump())
+        brand_dump.pop("brand_password")
+        inserted_user_data = {"_id": str(inserted_user.inserted_id), **brand_dump}
 
         access_token = create_access_token(
-            data={"sub": str(inserted_user.inserted_id)})
+            data={"brand_id": str(inserted_user.inserted_id)})
 
         return {
             "message":"User registered sucessfully!",
-            "access_token": access_token, 
-            "data": inserted_user_data
+            "data": {
+                "access_token": access_token,
+                "_id": str(inserted_user_data["_id"]),
+                "brand_name": inserted_user_data["brand_name"],
+                "brand_email": inserted_user_data["brand_email"]
+            }   
         }
         
     except Exception as e:
         raise handle_exception(e)
 
+
+@router.post("/check", dependencies=[Depends(authenticate_user)], status_code=status.HTTP_200_OK)
+async def authenticate_check(request: Request):
+    """
+    Authenticate user
+
+    Parameters:
+    - request (Request): the request object
+
+    Returns:
+    - dict: the response message
+    """
+    return {
+        "message": "User authenticated successfully",
+        "data": {
+            "_id": str(request.state.brand["_id"]),
+            "brand_name": request.state.brand["brand_name"],
+            "brand_email": request.state.brand["brand_email"]
+        }
+    }
 
 
 
