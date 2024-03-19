5ea5a5d6bb6855d1fc32947516b7352ac1ff7c85
Vaish24navi <diaandil2003@gmail.com>
2024-03-19 10:48:57 +0530

    feat(backend): FAQ APIs + uuid generation for schema

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,8 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-from api import auth
+from api import auth, faq
 from core.utils.middlewares import init_middlewares
-
-load_dotenv()
 
 app = FastAPI(
     title="Qreate",
@@ -15,23 +13,24 @@ app = FastAPI(
 
 init_middlewares(app)
 
+load_dotenv()
 MONGO_URL = os.getenv('M_URI')
 
 @app.on_event("startup")
 def startup_db_client():
-    mongodb_client = MongoClient(MONGO_URL)
-    database = mongodb_client["Qreate"]
+    app.mongodb_client = MongoClient(MONGO_URL, uuidRepresentation="standard")
+    database = app.mongodb_client["Qreate"]
     app.brands_collection = database.get_collection("brands")
-    server_info = mongodb_client.server_info()
+    server_info = app.mongodb_client.server_info()
     print(f"Connected to MongoDB server {server_info['version']}")
 
 @app.on_event("shutdown")
 def shutdown_db_client():
     app.mongodb_client.close()
 
-
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Qreate!"}
 
 app.include_router(auth.router)
+app.include_router(faq.router)
