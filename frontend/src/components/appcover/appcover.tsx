6574f358be9e89f81e51218ebeda3d86681d2ed7
niyasrad <niyasrad@gmail.com>
2024-03-20 10:16:11 +0530

    fix(frontend): patch for several found issues - 2

--- frontend/src/components/appcover/appcover.tsx
+++ frontend/src/components/appcover/appcover.tsx
@@ -1,7 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 import Navbar from "../navbar/navbar";
 import { AppcoverContainer, AppcoverDisplay } from "./appcover.styles";
+import Loading from "../loading/loading";
+
+import { useGlobalContext } from "../../contexts/global.context";
+
 
 export default function Appcover({ children }: { children: React.ReactNode }) {
+
+    const { isLoggedIn, isLoading, handleSignOut } = useGlobalContext()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!isLoggedIn && !isLoading) {
+            handleSignOut!()
+            navigate('/sign-in')
+        }
+    }, [isLoggedIn, isLoading])
+    
+
+    if (isLoading || !isLoggedIn) return <Loading />
+
     return (
         <AppcoverContainer>
             <Navbar></Navbar>
